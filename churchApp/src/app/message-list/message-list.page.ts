ae8645f62a536538d20452f9dd6bfeb7004fe5b9
Sabrina Ojea <72868271+SabrinaOC@users.noreply.github.com>
2024-05-26 23:23:50 +0200

    chore: msg type service and app launcher

--- churchApp/src/app/message-list/message-list.page.ts
+++ churchApp/src/app/message-list/message-list.page.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Message } from '../model/interfaces';
 import { RestService } from '../services/rest.service';
-import { LoadingController, Platform } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
+import { AppLauncher, AppLauncherPlugin } from '@capacitor/app-launcher';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.page.html',
   styleUrls: ['./message-list.page.scss'],
 })
+
 export class MessageListPage implements OnInit {
   messageList!: Message[]
   isDesktop: boolean = false;
   constructor(
               private restService: RestService,
-              private platform: Platform,
-              private loadingController: LoadingController
+              private loadingController: LoadingController,
   ) { }
 
   ngOnInit() {
-    
+    console.log('init')
   }
 
   async ionViewWillEnter() {
@@ -29,10 +30,14 @@ export class MessageListPage implements OnInit {
     console.log('ionViewWillEnter')
     this.restService.getAllMessages().subscribe((data: any) => {
       if(data) {
-        this.messageList = data.messageList
+        this.messageList = data.messageListMapped
       }
       loading.dismiss()
     })
+  }
+
+  openUrl(targetUrl: string) {
+    AppLauncher.openUrl({url: targetUrl})
   }
 
 }
--- churchApp/src/app/model/interfaces.ts
+++ churchApp/src/app/model/interfaces.ts
@@ -4,20 +4,18 @@ export interface Book {
     name: string,
     description: string
 }
-export interface Message {
-    id: number,
-    // id_speaker: number,
-    title: string,
-    url: string,
-    date: Date,
-    // id_book: number,
-    Speaker: Speaker,
-    Book: Book
-}
 
 export interface Speaker {
     id: number,
     name: string
+}
+export interface Message {
+    id: number,
+    title: string,
+    url: string,
+    date: Date,
+    speaker: Speaker,
+    book: Book
 }
 
 export type NewMessage = Omit<Message, 'id'>
