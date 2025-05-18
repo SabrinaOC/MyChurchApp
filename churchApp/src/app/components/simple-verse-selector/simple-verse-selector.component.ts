23db6813d348302e0409e16419048628e32f259a
David Padilla <davidpaag2002@gmail.com>
2025-05-18 21:41:13 +0200

    feat: serveral app fixes

--- churchApp/src/app/components/simple-verse-selector/simple-verse-selector.component.ts
+++ churchApp/src/app/components/simple-verse-selector/simple-verse-selector.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
+import { Book } from 'src/app/services/api/models';
 import { CoreProvider } from 'src/app/services/core';
 
 @Component({
@@ -9,38 +10,52 @@ import { CoreProvider } from 'src/app/services/core';
 })
 export class SimpleVerseSelectorComponent {
 
-  books: string[] = this.core.getAllBibleBooks()
-  filteredBooks: string[] = [...this.books]; // Lista filtrada
+  @Input() public justBook: boolean = false;
+  
+  filteredBooks: Book[] = [...this.core.bookList]; // Filtered list
   searchTerm: string = '';
-  selectedBook: string = '';
+  selectedBook: Book | undefined;
 
   chapters: number = 0;
   selectedChapter: number = 0;
   selectedVerse: number = 0;
   selectedRange: number = 0;
 
-  verses: number = 0;
+  chapterVerses: number = 0;
 
   @Output() public verse: EventEmitter<any> = new EventEmitter();
 
   constructor(public core: CoreProvider, private popoverCtrl: PopoverController) { }
 
   filterOptions() {
-    this.filteredBooks = this.books.filter(book =>
-      book.toLowerCase().includes(this.searchTerm.toLowerCase())
+    this.filteredBooks = this.core.bookList.filter(book =>
+      book.name?.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
   }
 
-  selectBook(book: string) {
+  selectBook(book: Book) {
     this.selectedBook = book;
-    this.searchTerm = book; // Show selected value at toolbar
-    this.chapters = this.core.getChapterCount(this.selectedBook) ?? 0;
+    this.searchTerm = book.name ?? ''; // Show selected value at toolbar
+    this.chapters = this.core.getChapterCount(this.selectedBook.name!) ?? 0;
+
+    if (this.justBook) {
+      this.chooseVerse();
+    }
   }
 
   selectChapter(chapter: number) {
     this.selectedChapter = chapter;
 
-    this.verses = this.core.getVerses(this.selectedBook, this.selectedChapter) ?? 0;
+    this.chapterVerses = this.core.getVerses(this.selectedBook!.name!, this.selectedChapter) ?? 0;
+  }
+
+  selectVerse(verse: number) {
+    this.selectedVerse = verse;
+
+    //If the selectedVerse is the last of the chapter, exit
+    if (verse == this.chapterVerses) {
+      this.chooseVerse();
+    }
   }
 
   selectRange(range: number) {
@@ -54,7 +69,13 @@ export class SimpleVerseSelectorComponent {
 
 
   chooseVerse() {
-    let verse = `${this.selectedBook} ${this.selectedChapter}:${this.selectedVerse}${this.selectedRange === 0 ? "" : "-" + this.selectedRange}`;
+    let verse: string | Book = "";
+    
+    if (this.justBook) {
+      verse = this.selectedBook!;
+    } else {
+      verse = `${this.selectedBook?.name} ${this.selectedChapter}:${this.selectedVerse}${this.selectedRange === 0 ? "" : "-" + this.selectedRange}`;
+    }
 
     this.popoverCtrl.dismiss(verse);
   }
