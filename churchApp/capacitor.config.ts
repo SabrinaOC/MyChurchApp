fb95a344dec89dcdb1b24846a538a33461712c11
David Padilla <davidpaag2002@gmail.com>
2026-02-09 20:42:12 +0100

    feat(app): Renaming app name

--- churchApp/capacitor.config.ts
+++ churchApp/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'churchApp',
+  appId: 'io.my.church',
+  appName: 'My Church',
   webDir: 'www'
 };
 
